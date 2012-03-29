094b213bc79a760be5403fa5a154bced7be93c61
Stephen <beholdthepanda@gmail.com>
2012-03-29 01:15:07 -0400

    basic registry page

--- js/application.js
+++ js/application.js
@@ -1,7 +1,19 @@
+
 $(document).ready(function(){
+  function justShow (guytoshow) {
+    $('.stage_item').fadeOut(250);
+    window.setTimeout(function(){guytoshow.fadeIn(250)}, 300);
+  }
+  
   $('#show_mailing_address').click(function(){
+    $('.stage_item').fadeOut(250);
     var addressbox = $("#mailing_address");
     addressbox.html("<strong>Stephen Schor c/o Eastmedia.com</strong><br/> 27 West 24th Street Suite 703 <br/>New York, NY 10010");
     addressbox.fadeIn(300);
+  })
+  
+  $("#paypalform").change(function(){
+    var selected_value = $("#paypalform option:selected").val();
+    justShow($(document.getElementById(selected_value)));
   })
 });
