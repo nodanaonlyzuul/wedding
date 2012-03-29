febb82e1d61cfacf72dcabe0efab40f1539d21f4
nodanaonlyzuul <stephen@eastmedia.com>
2012-03-29 11:15:16 -0400

    minor style changes

--- js/application.js
+++ js/application.js
@@ -5,15 +5,15 @@ $(document).ready(function(){
     $('.stage_item').fadeOut(250);
     window.setTimeout(function(){guytoshow.fadeIn(250)}, 300);
   }
-  
+
   $('#show_mailing_address').click(function(){
-    $("#stage").show();
+    $("#stage").show(250);
     $('.stage_item').fadeOut(250);
     var addressbox = $("#mailing_address");
     addressbox.html("<strong>Stephen Schor c/o Eastmedia.com</strong><br/> 27 West 24th Street Suite 703 <br/>New York, NY 10010");
     addressbox.fadeIn(300);
   })
-  
+
   $("#paypalform").change(function(){
     var selected_value = $("#paypalform option:selected").val();
     justShow($(document.getElementById(selected_value)));
